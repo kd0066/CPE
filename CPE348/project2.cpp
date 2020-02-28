d5d425f20669027c3797c9a8bcc29492af91b610
kd0066 <kd0066@uah.edu>
2020-02-27 21:09:44 -0600

    I hope this doesnt break everything

--- CPE348/project2.cpp
+++ CPE348/project2.cpp
@@ -1,7 +1,3 @@
-/**********************Kevin Donovan***********************
-*******CPE 348 Simulated exponential backoff protocol******
-***********************************************************/
-
 #include <iostream>  // inputs and outputs
 using namespace std;
 #include <iomanip>
@@ -14,66 +10,67 @@ int main()
 {
 
 	long int timeout;
-	int mod[6] = {2};
-	int slot[6] = {0};
-	bool checkMe[6] = {false};
+	int mod[6] = {2, 2, 2, 2, 2, 2};
+	int slot[6] = {0, 0, 0, 0, 0, 0};
 	int wait = 0;
 	int check = 0;
 	int count = 0;
+	int f = 0;
+	int i = 0;
 
 	ifstream inFile;
 	string file_name;
 	inFile.open("Project2_part1_rn.txt");
 
-	while (i != 6) {
-		if (count > 5) {
-			for (int j = 0; j < 6; j++) {
-				cout << j << endl;
-				for (int s = 0; s < 6; s++) {
-					if (j != s) {
-						if (slot[j] == slot[s]) {
-							checkMe[j] = false;
-							j++;
-						}
-						else {
-							checkMe[j] = true;
-						}
-					}
-					j++;
-				}
-			}
-			count = 0;
-			check++;
-			cout << endl;
-		}
-
+	while (f < 6) {
 		if (slot[count] == check) {
 			inFile >> timeout;
 			wait = timeout % mod[count];
-
 			slot[count] = wait + slot[count] + 1;
-			cout << "Signal " << count << " is moved to slot " << slot[count] << " with a timeout value of " << timeout << " " << checkMe[count] << endl;
+			cout << "Signal " << count << " is moved to slot " << slot[count] << " with a timeout value of " << timeout << " " << endl;
 			mod[count] = mod[count] * 2;
 			count++;
-			i++;
-			if(checkMe[count]) {
-				slot[count] = 0;
-				i++;
-				cout << i << endl;
 
-			}
 		}
+		else if (slot[count] == check) {
+			inFile >> timeout;
+			wait = timeout % mod[count];
+			slot[count] = wait + slot[count] + 1;
+			cout << "Signal " << count << " is moved to slot " << slot[count] << " with a timeout value of " << timeout << " " << endl;
+			mod[count] = mod[count] * 2;
+			count++;
 
-
+		}
 		else {
 			count++;
 		}
 
+		if (count > 5) {
+			for (int j = 0; j < 6; j++) {
+				for (int s = 0; s < 6; s++) {
+					if (j != s) {
+						if (slot[j] == slot[s]) {
+							i = 0;
+							break;
+						}
+						else {
+							i++;
+							if (i == 5) {
+								cout << j << endl;
+								i = 0;
+								f++;
+							}
+							continue;
+						}
+					}
+				}
+			}
 
-
+			count = 0;
+			check++;
+			cout << endl;
+		}
 	}
-
-
-	// inFile.close();
-	// return 0;
+	i = 0;
+	return 0;
 }
